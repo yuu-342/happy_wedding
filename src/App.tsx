5befe4b5ee853d7133bfac60e96000068be18181
kshidara <kshidara@yahoo-corp.jp>
2021-08-29 04:59:55 +0900

    ぽこぽこ

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { data } from "./sample/mock-responce.";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [isShow, setIsShow] = useState(false);
+  const onClick = () => {
+    setIsShow(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main">
+      <button type="button" onClick={onClick}>
+        結婚おめでとう
+      </button>
+
+      <div className="MessageList">
+        {/* 繰り返し要素 */}
+        {data.map((item, index) => (
+          <div
+            className="Column"
+            key={index}
+            style={{
+              top: `${Math.floor(Math.random() * 100) + index}%`,
+              left: `${Math.floor(Math.random() * 60)}%`,
+              animationDelay: `${index * 0.2}s`,
+            }}
+          >
+            <div className="Column__item">
+              <img
+                src={item.image}
+                width=""
+                height=""
+                alt="写真"
+                className="Thumbnail"
+              />
+              {/* <img src={item.image} width="" height="" alt="写真" /> */}
+            </div>
+            {item.comment && item.comment !== "" && (
+              <div className="Column__item">
+                <p className="Comment">{item.comment}</p>
+              </div>
+            )}
+          </div>
+        ))}
+        {/* 繰り返し要素 */}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/sample/mock-responce..ts
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    // image:
+    //   "https://drive.google.com/file/d/1nerX9FWq5cXWWCnmuGw-4jN_S4XiAg4J/view?usp=sharing",
+    image:
+      "https://tk.ismcdn.jp/mwimgs/4/a/1140/img_4a30e7236c831a031f5b536bc5ea490628815.jpg",
+    comment: "ご結婚おめでとうございます❗️末永くお幸せに〜",
+  },
+  {
+    // image:
+    //   "https://drive.google.com/file/d/1nerX9FWq5cXWWCnmuGw-4jN_S4XiAg4J/view?usp=sharing",
+    image:
+      "https://tk.ismcdn.jp/mwimgs/4/a/1140/img_4a30e7236c831a031f5b536bc5ea490628815.jpg",
+    comment: "ご結婚おめでとうございます❗️末永くお幸せに〜",
+  },
+  {
+    // image:
+    //   "https://drive.google.com/file/d/1nerX9FWq5cXWWCnmuGw-4jN_S4XiAg4J/view?usp=sharing",
+    image:
+      "https://tk.ismcdn.jp/mwimgs/4/a/1140/img_4a30e7236c831a031f5b536bc5ea490628815.jpg",
+    comment: "ご結婚おめでとうございます❗️末永くお幸せに〜",
+  },
+  {
+    // image:
+    //   "https://drive.google.com/file/d/1nerX9FWq5cXWWCnmuGw-4jN_S4XiAg4J/view?usp=sharing",
+    image:
+      "https://tk.ismcdn.jp/mwimgs/4/a/1140/img_4a30e7236c831a031f5b536bc5ea490628815.jpg",
+    comment: "ご結婚おめでとうございます❗️末永くお幸せに〜",
+  },
+  {
+    // image:
+    //   "https://drive.google.com/file/d/1nerX9FWq5cXWWCnmuGw-4jN_S4XiAg4J/view?usp=sharing",
+    image:
+      "https://tk.ismcdn.jp/mwimgs/4/a/1140/img_4a30e7236c831a031f5b536bc5ea490628815.jpg",
+    comment: "ご結婚おめでとうございます❗️末永くお幸せに〜",
+  },
+  {
+    // image:
+    //   "https://drive.google.com/file/d/1nerX9FWq5cXWWCnmuGw-4jN_S4XiAg4J/view?usp=sharing",
+    image:
+      "https://tk.ismcdn.jp/mwimgs/4/a/1140/img_4a30e7236c831a031f5b536bc5ea490628815.jpg",
+    comment: "ご結婚おめでとうございます❗️末永くお幸せに〜",
+  },
+];
